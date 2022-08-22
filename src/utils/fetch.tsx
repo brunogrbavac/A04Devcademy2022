1af83f5616c0a7abe9f057a72c1ff0b8306b7674
Bruno Grbavac <bruno.grbavac@king-ict.hr>
2022-08-22 23:54:11 +0200

    Minor fetch changes

--- src/utils/fetch.tsx
+++ src/utils/fetch.tsx
@@ -7,13 +7,16 @@ export const fetchData = async (url: string) => {
 
 export const postData = async (url: string, payload: Object) => {
     return await fetch(url, { method: "POST", body: JSON.stringify(payload), headers: { "Content-type": "application/json; charset=UTF-8" } })
+    .catch(error => alert(error.message));
 };
 
 export const putData = async (url: string, payload: Object) => {
     return await fetch(url, { method: "PUT", body: JSON.stringify(payload), headers: { "Content-type": "application/json; charset=UTF-8" } })
+    .catch(error => alert(error.message));
 };
   
 export const deleteData = async (url: string) => {
     return await fetch(url, { method: "DELETE", headers: { "Content-type": "application/json; charset=UTF-8" } })
+    .catch(error => alert(error.message));
 };
   
